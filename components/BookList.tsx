01ea8aec674e056e63ffabfd5af21afbd883d6d4
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-06-21 16:07:21 +0700

    add component BookList and Book Overview

---
+++ components/BookList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <section>
+      <h2 className="font-bebas-neue text-light-100 text-4xl">Popular Books</h2>
+    </section>
+  );
+};
+
+export default BookList;
