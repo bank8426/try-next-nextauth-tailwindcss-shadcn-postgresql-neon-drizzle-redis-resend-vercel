97e3a36243b03d80f4d8d2622cc94ba27ddc9b16
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-06-24 14:49:09 +0700

    handle update user's lastActivityDate once per day, check active user based on lastActivityDate, trigger workflowClient when user signup

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,39 @@
 import { Client as WorkflowClient } from "@upstash/workflow";
+import { Client as QStashClient } from "@upstash/qstash";
 import config from "./config";
+
+const qstashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
+});
 
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  console.error("Send email not implemented");
+
+  // TODO
+  // await qstashClient.publishJSON({
+  //   // api: {
+  //   //   name: "email",
+  //   //   // provider: "https://firstqstashmessage.requestcatcher.com/test",
+  //   // },
+  //   // url: "https://firstqstashmessage.requestcatcher.com/test",
+  //   body: {
+  //     from: "Gu Bank <gubankcpe.dev@gmail.com>",
+  //     to: [email],
+  //     subject: "Helloo",
+  //     html: "<p>World</p>",
+  //   },
+  // });
+};
