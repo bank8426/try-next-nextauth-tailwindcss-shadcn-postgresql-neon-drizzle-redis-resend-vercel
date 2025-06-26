bec8f0120f8b61df62260fd7f0dbe4a67cab2627
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-06-26 14:40:53 +0700

    add BorrowBookButton componet, handle borrow book on book overview component

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import BookCover from "./BookCover";
-const BookOverview = ({
+import BorrowBookButton from "./BorrowBookButton";
+import { db } from "@/database/drizzle";
+import { usersTable } from "@/database/schema";
+import { eq } from "drizzle-orm";
+
+interface Props extends Book {
+  userId: string;
+}
+
+const BookOverview = async ({
   title,
   author,
   genre,
@@ -12,7 +21,23 @@ const BookOverview = ({
   description,
   coverColor,
   coverUrl,
-}: Book) => {
+  id,
+  userId,
+}: Props) => {
+  const [user] = await db
+    .select()
+    .from(usersTable)
+    .where(eq(usersTable.id, userId))
+    .limit(1);
+
+  const borrowingEligibility = {
+    isEligible: availableCopies > 0 && user?.status === "APPROVED",
+    message:
+      availableCopies <= 0
+        ? "Book is not available"
+        : "You are not eligible to borrow this book",
+  };
+
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
@@ -44,10 +69,13 @@ const BookOverview = ({
         </div>
         <p className="book-description">{description}</p>
 
-        <Button className="book-overview_btn">
-          <Image src="/icons/book.svg" alt="book" width={20} height={20} />
-          <p className="font-bebas-neue text-dark-100 text-xl">Borrow</p>
-        </Button>
+        {user && (
+          <BorrowBookButton
+            bookId={id}
+            userId={userId}
+            borrowingEligibility={borrowingEligibility}
+          />
+        )}
       </div>
       <div className="relative flex flex-1 justify-center">
         <div className="relative">
---
+++ components/BorrowBookButton.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { useState } from "react";
+import { toast } from "sonner";
+import { borrowBook } from "@/lib/actions/book";
+
+interface Props {
+  bookId: string;
+  userId: string;
+  borrowingEligibility: {
+    isEligible: boolean;
+    message: string;
+  };
+}
+
+const BorrowBookButton = ({
+  bookId,
+  userId,
+  borrowingEligibility: { isEligible, message },
+}: Props) => {
+  const router = useRouter();
+  const [borrowing, setBorrowing] = useState(false);
+  const handleBorrow = async () => {
+    if (!isEligible) {
+      toast.error("Error", { description: message });
+    }
+
+    setBorrowing(true);
+
+    try {
+      const result = await borrowBook({ bookId, userId });
+
+      if (result.success) {
+        toast.success("Success", { description: "Book borrowed successfully" });
+        router.push("/");
+      } else {
+        toast.error("Error", {
+          description: result.error,
+        });
+      }
+    } catch (error) {
+      toast.error("Error", {
+        description: "An error occurred while borrowing the book",
+      });
+    } finally {
+      setBorrowing(false);
+    }
+  };
+
+  return (
+    <Button
+      className="book-overview_btn"
+      onClick={handleBorrow}
+      disabled={borrowing}
+    >
+      <Image src="/icons/book.svg" alt="book" width={20} height={20} />
+      <p className="font-bebas-neue text-dark-100 text-xl">
+        {borrowing ? "Borrowing ..." : "Borrow Book"}
+      </p>
+    </Button>
+  );
+};
+
+export default BorrowBookButton;
